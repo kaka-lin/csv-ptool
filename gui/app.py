c3adcbf9a8814645e74d6744042cc775e997745a
kaka <vn503024@gmail.com>
2017-09-22 11:32:06 +0800

    Add: 新增支援翻譯功能雛形

--- gui/app.py
+++ gui/app.py
@@ -1,11 +1,16 @@
 import sys
+from PyQt5.QtCore import QTranslator, QLocale
 from PyQt5.QtWidgets import QApplication
 from gui.mainwindow import MainWindow
 
 def run():
+    
+    translator = QTranslator()
+    #translator.load(QLocale, 'csvtool', '_', './i18n')
+    
     app = QApplication(sys.argv)
 
-    main_window = MainWindow(app)
+    main_window = MainWindow(app, translator)
     main_window.show()
 
     sys.exit(app.exec_())
